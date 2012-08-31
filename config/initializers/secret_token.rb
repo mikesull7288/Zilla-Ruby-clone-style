2925d686a156bc42b95c8ab251f313e25f2e322d
maksim demin <mdemin914@gmail.com>
2012-08-30 19:48:41 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyZilla::Application.config.secret_token = 'abe131170366f1ffbc1d26ce72995f2faf035df5dcaa8eaad5cc87069c7d412bdfab11da5c7249ff3deb23d5d1d1b790242252f6bc86ff81f3860d7c8a51e907'
