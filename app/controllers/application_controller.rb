d94e038c94289f39a3db3bfdfc7ead12ea961028
mdemin914@gmail.com <mdemin914@gmail.com>
2013-05-27 18:47:41 -0700

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #Zuora.configure(username: "smogger914@yahoo.com", password: "Fo!d3168", sandbox: true, logger: true)
-  ZillaBackend::Config.initialize(username: "smogger914@yahoo.com", pass: "Fo!d3168", sandbox: true, logger: true)
+  ZillaBackend::Config.initialize(username: "smogger914@yahoo.com", pass: "Zuora002", sandbox: true, logger: true)
 
   
 end
